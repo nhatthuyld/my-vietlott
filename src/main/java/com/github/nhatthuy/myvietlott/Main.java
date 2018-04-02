cba8159ec0e067734147a1106018eaf7bd537dcb
NhatThuy <nhatthuyld@gmail.com>
2018-04-02 15:00:24 +0700

    inital

---
+++ src/main/java/com/github/nhatthuy/myvietlott/Main.java
@@ -0,0 +1,9 @@
+package com.github.nhatthuy.myvietlott;
+
+import java.util.Date;
+
+public class Main{
+	public static  void main (String args[]) {
+		System.out.println("Time is: "+ new Date().getTime());
+	}
+}
